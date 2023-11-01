47a46ad7b2ae00d47f1acaf784510eb1d449e228
Ruslan Gumirov <gumirus29@gmail.com>
2023-11-01 19:25:53 +0500

    webpck_study-lessons

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import data from './data'
+import './index.scss'
+
+const root = document.querySelector('#app')
+
+function renderItem(item, index) {
+  const li = document.createElement('li')
+  li.textContent = item.title
+  if (index % 2 === 0) {
+    li.style.color = 'red'
+  }
+  root.append(li)
+}
+
+data.forEach(renderItem)
